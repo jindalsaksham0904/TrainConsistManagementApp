f090a96148be465ba8b9d4c0959dfcb4e6b5df0a
SakshamJindal04 <sakshamjindal1620@gmail.com>
2026-04-04 19:04:57 +0530

    UC7: Sort Bogies using Comparator

---
+++ App/src/UseCase7Comparator.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Bogie {
+    String name;
+    int capacity;
+
+    Bogie(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+}
+
+public class UseCase7Comparator {
+
+    public static void main(String[] args) {
+
+        List<Bogie> list = new ArrayList<>();
+
+        list.add(new Bogie("Sleeper", 72));
+        list.add(new Bogie("AC Chair", 56));
+        list.add(new Bogie("First Class", 24));
+
+        list.sort(Comparator.comparingInt(b -> b.capacity));
+
+        for (Bogie b : list) {
+            System.out.println(b.name + " → " + b.capacity);
+        }
+    }
+}
